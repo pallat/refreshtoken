d1c0280f3d4a939770791b7f307136a542839903
pallat <yod.pallat@gmail.com>
2018-01-31 16:20:47 +0700

    refresh token by expiresAt

---
+++ refresh.go
@@ -0,0 +1,74 @@
+package refreshtoken
+
+import (
+	"sync"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+const (
+	hmacSampleSecret = "qvjqnhzu8LMWDCmu7MgvDpaMWsASAmhd"
+)
+
+type tokenizer interface {
+	Get() (jwtToken, error)
+}
+
+type tokenize struct {
+	t           tokenizer
+	tokenString string
+	mux         sync.Mutex
+}
+
+func Tokenize() *tokenize {
+	t := &tokenize{
+		t:           &tokenClient{lifeTime: 1},
+		tokenString: "",
+		mux:         sync.Mutex{},
+	}
+
+	go t.Start()
+
+	return t
+}
+
+func (t *tokenize) Start() {
+	token, _ := t.t.Get()
+	t.tokenString = token.TokenString
+	marking := token.ExpiresAt
+
+	for range time.Tick(time.Duration(marking) - (500 * time.Millisecond)) {
+		token, _ := t.t.Get()
+		t.mux.Lock()
+		t.tokenString = token.TokenString
+		t.mux.Unlock()
+	}
+}
+
+func (t *tokenize) Token() string {
+	return t.tokenString
+}
+
+type tokenClient struct {
+	lifeTime time.Duration
+}
+
+type jwtToken struct {
+	TokenString string `json:"tokenString"`
+	ExpiresAt   int64  `json:"expiresAt"`
+}
+
+func (t *tokenClient) Get() (jwtToken, error) {
+	claims := &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(t.lifeTime * time.Second).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(hmacSampleSecret))
+	// Sign and get the complete encoded token as a string using the secret
+	return jwtToken{
+		TokenString: tokenString,
+		ExpiresAt:   claims.ExpiresAt,
+	}, err
+}
---
+++ refresh_test.go
@@ -0,0 +1,63 @@
+package refreshtoken
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type fakeTokenClient struct{}
+
+func (fakeTokenClient) Get() (jwtToken, error) {
+	claims := &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(1 * time.Second).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(hmacSampleSecret))
+	// Sign and get the complete encoded token as a string using the secret
+	return jwtToken{
+		TokenString: tokenString,
+		ExpiresAt:   claims.ExpiresAt,
+	}, err
+}
+
+func TestGetToken(t *testing.T) {
+	tkn := tokenize{
+		t:           &fakeTokenClient{},
+		tokenString: "",
+	}
+
+	go tkn.Start()
+	<-time.After(200 * time.Millisecond)
+
+	tk := tkn.Token()
+	if tk == "" {
+		t.Error("it should return someting like token")
+	}
+}
+
+func TestGetTokenExpiredShouldRefreshAutomaticly(t *testing.T) {
+	tkn := Tokenize()
+	tkn.t = fakeTokenClient{}
+	tokenString := tkn.Token()
+	fmt.Println(tokenString)
+
+	<-time.After(2 * time.Second)
+	tokenString = tkn.Token()
+	fmt.Println(tokenString)
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(hmacSampleSecret), nil
+	})
+
+	fmt.Println(token, err)
+}
